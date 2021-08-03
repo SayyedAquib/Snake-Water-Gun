a4e00b20e874d6bd10aa1004f05e6dc42db3d11e
Sayyed Aquib <72184419+SayyedAquib@users.noreply.github.com>
2021-08-03 18:22:46 +0530

    Add files via upload

---
+++ SnakeWaterGun/main.py
@@ -0,0 +1,45 @@
+print("SNAKE-WATER-GUN")
+human_point = 0
+computer_point = 0
+i = 9
+while i >= 1:
+    import random
+
+    lst = ["s", "w", "g"]
+    choice = random.choice(lst)
+    a = (input("s for Snake \nw for Water \ng for Gun \nEnter your choice: "))
+    print(choice)
+    if a == choice:
+        print("DRAW")
+    elif a == "s" and choice == "w":
+        print("You win.")
+        human_point += 1
+    elif a == "s" and choice == "g":
+        print("You lose.")
+        computer_point += 1
+    elif a == "w" and choice == "s":
+        print("You lose.")
+        computer_point += 1
+    elif a == "w" and choice == "g":
+        print("You win.")
+        human_point += 1
+    elif a == "g" and choice == "s":
+        print("You win.")
+        human_point += 1
+    elif a == "g" and choice == "w":
+        print("You lose.")
+        computer_point += 1
+    else:
+        print("Please type valid input.")
+
+    print(f"{i} chances are left out of 10")
+    i -= 1
+
+print(human_point)
+print(computer_point)
+if human_point > computer_point:
+    print("YOU WIN THIS GAME.")
+elif human_point == computer_point:
+    print("THE GAME IS DRAW. \nPlay agian.")
+else:
+    print("YOU LOSE THIS GAME.")
